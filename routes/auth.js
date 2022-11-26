b987b0657e7260179fde9f171014f351fccb345a
Anurag Jha <74069648+anurag1jha@users.noreply.github.com>
2022-11-26 21:35:44 +0530

    youtube-BE

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 import express from "express";
-import {signin, signup} from "../controllers/auth.js";
+import { googleAuth, signin, signup } from "../controllers/auth.js";
 
-const router = express .Router();
+const router = express.Router();
 
-// CREATE A USER
-router.post("/signup",signup)
+//CREATE A USER
+router.post("/signup", signup)
+
 //SIGN IN
-router.post("/signin",signin)
+router.post("/signin", signin)
+
 //GOOGLE AUTH
-router.post("/signin",)
+router.post("/google", googleAuth)
 
 export default router;
